c6c33b9fa0f2bbfff08b35c068ca1a64c912752e
zhuangjing <1341036862@qq.com>
2019-04-09 23:55:07 +0800

    jenkins

--- src/main/java/com/zhuangjing/jenkinstest/TestController.java
+++ src/main/java/com/zhuangjing/jenkinstest/TestController.java
@@ -2,12 +2,20 @@ package com.zhuangjing.jenkinstest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
+@RestController
 public class TestController {
 
     @GetMapping("/hello")
-    public void hello(){
-        log.info("hello");
+    public Object hello(){
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "zhuagnjing");
+        return map;
     }
 }
