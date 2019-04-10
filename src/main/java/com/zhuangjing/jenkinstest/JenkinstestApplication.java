8f078ff04844e7f1ad2ea3deff6997ea1d6884c2
zhuangjing <1341036862@qq.com>
2019-04-10 09:05:15 +0800

    jenkins

--- src/main/java/com/zhuangjing/jenkinstest/JenkinstestApplication.java
+++ src/main/java/com/zhuangjing/jenkinstest/JenkinstestApplication.java
@@ -2,12 +2,20 @@ package com.zhuangjing.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinstestApplication {
+public class JenkinstestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinstestApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinstestApplication.class);
+    }
+
 
 }
